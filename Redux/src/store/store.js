2781676dce31f4d5c23d7e2846d21b6022ed7d32
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-05-11 12:14:46 +0100

    "Updated store creation in Redux using '@reduxjs/toolkit' configureStore and imported 'counterSlice.reducer'"
    Explanation:
    
    * The code diff shows that the import statement has been updated to include '@

--- Redux/src/store/store.js
+++ Redux/src/store/store.js
@@ -1,8 +1,11 @@
 import { createStore } from "redux";
 import { counterReducer } from "../reducer/counterReducer";
 import { counterSlice } from "../Slice/countSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
 //const store = createStore(counterReducer);
-const store = createStore(counterSlice.reducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
