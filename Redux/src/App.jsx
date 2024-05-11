b2b2d329197cd694f2581a20ba1528ab8c6566c0
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-05-11 13:15:18 +0100

    "Added user authentication and profile components using Redux"
    This diff adds user authentication and profile components to the application using Redux. The Auth component now includes a form for logging in, and the Header component includes a navigation menu with a log

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,13 +1,17 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuth);
+
   return (
     <>
       <Header />
-      <Auth />
+      {!isAuth ? <Auth /> : <UserProfile />}
       <Counter />
     </>
   );
