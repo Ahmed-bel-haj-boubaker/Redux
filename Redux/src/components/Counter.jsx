e2125980f10b08db585be669f85c46e7fd5aaa3a
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-05-11 11:37:57 +0100

    "Add 'Increment By 5' button and corresponding increase action in counterReducer"
    This diff adds a new button to the Counter component that dispatches an "increase" action when clicked. The increase action

--- Redux/src/components/Counter.jsx
+++ Redux/src/components/Counter.jsx
@@ -11,6 +11,10 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
   const toggleCounterHandler = () => {};
   const counter = useSelector((state) => state.counter);
   return (
@@ -20,6 +24,7 @@ const Counter = () => {
         <div className={classes.value}>{counter}</div>
         <div>
           <button onClick={() => incrementHandler()}>Increment</button>
+          <button onClick={() => increaseHandler()}>Increment By 5</button>
           <button onClick={() => decrementHandler()}>Decrement</button>
         </div>
         <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- Redux/src/reducer/counterReducer.js
+++ Redux/src/reducer/counterReducer.js
@@ -4,6 +4,11 @@ export const counterReducer = (state = { counter: 0 }, action) => {
       counter: state.counter + 1,
     };
   }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+    };
+  }
 
   if (action.type === "decrement") {
     return {
