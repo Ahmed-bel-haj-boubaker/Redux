f38567fb1dd646e60dabf72c31e8a083f3628328
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-05-11 12:11:20 +0100

    connect slice reducer with the store

--- Redux/src/store/store.js
+++ Redux/src/store/store.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
 import { counterReducer } from "../reducer/counterReducer";
+import { counterSlice } from "../Slice/countSlice";
 
-const store = createStore(counterReducer);
+//const store = createStore(counterReducer);
+const store = createStore(counterSlice.reducer);
 
 export default store;
